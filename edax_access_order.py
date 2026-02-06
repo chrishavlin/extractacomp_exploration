3e1755907a8960b34610a57ad66f19cba06f385e
chavlin <chris.havlin@gmail.com>
2026-02-06 10:03:22 -0600

    more memray testing

--- edax_access_order.py
+++ edax_access_order.py
@@ -1,6 +1,6 @@
 from rsciio import edax
 import os
-import numpy as np 
+import numpy as np
 from typing import Any
 import numpy.typing as npt
 import argparse
@@ -10,90 +10,99 @@ datadir = os.environ.get("EXTRACTACOMPDATADIR")
 
 
 def load_edax_spd(basename: str) -> dict[str, Any]:
-    subdir = os.path.join(datadir, basename, "Proprietary EDAX Files")    
+    subdir = os.path.join(datadir, basename, "Proprietary EDAX Files")
     fispd = os.path.join(subdir, f"{basename}.spd")
     fiipr = os.path.join(subdir, f"{basename}.ipr")
-    
+
     ds = edax.file_reader(fispd, ipr_fname=fiipr)[0]
-    return ds  
+    return ds
 
 
 
-def sum_over_energy_index_range(filename_base: str, 
-                                index_start: int, 
+def sum_over_energy_index_range(filename_base: str,
+                                index_start: int,
                                 index_end: int,):
-    
-    # do NOT pass the memmap handle in. data still 
-    # accumulates! 
+
+    # do NOT pass the memmap handle in. data still
+    # accumulates!
     # https://stackoverflow.com/a/61472122/9357244
     ds = load_edax_spd(filename_base)
     subsample = np.array(ds['data'][:,:,index_start: index_end])
     return subsample.sum(axis=0).sum(axis=0)
-    
+
 
 def get_n_chunks(filename_base: str, chunksize: int):
-    ds = load_edax_spd(filename_base)  
-    n_chunks = int(np.ceil(ds['axes'][2]['size'] / chunksize))    
+    ds = load_edax_spd(filename_base)
+    n_chunks = int(np.ceil(ds['axes'][2]['size'] / chunksize))
     maxindex = ds['axes'][2]['size']
     return maxindex, n_chunks
 
 def integrate_spatial_dims(filename_base: str, chunksize: int) -> npt.NDArray[np.floating]:
-    
-    maxindex, n_chunks = get_n_chunks(filename_base, chunksize)    
+
+    maxindex, n_chunks = get_n_chunks(filename_base, chunksize)
     channel_sums = np.zeros((maxindex,))
 
     print(f"integrating {n_chunks} chunks")
 
     for ichunk in range(n_chunks):
         channel_start = ichunk * chunksize
-        channel_end = channel_start + chunksize 
+        channel_end = channel_start + chunksize
         if channel_end > maxindex:
-            channel_end = maxindex 
+            channel_end = maxindex
         sumvalues = sum_over_energy_index_range(filename_base, channel_start, channel_end)
         channel_sums[channel_start:channel_end] = sumvalues
 
-    return channel_sums 
+    return channel_sums
 
 
-def run_test(filename_base: str, axis: int):  
-    ds = load_edax_spd(filename_base)       
+def run_test(filename_base: str, axis: int, reload_memmap: bool = False):
+    ds = load_edax_spd(filename_base)
 
     # sz_0 = ds['axes'][0]['size']
     # sz_1 = ds['axes'][1]['size']
     # sz_2 = ds['axes'][2]['size']
-    
-    chunksize = 50 
 
-    n_chunks = int(np.ceil(ds['axes'][axis]['size'] / chunksize)) 
-    maxindex = ds['axes'][axis]['size']    
-    del ds 
+    chunksize = 1
+
+    n_chunks = int(np.ceil(ds['axes'][axis]['size'] / chunksize))
+    maxindex = ds['axes'][axis]['size']
+
+    if reload_memmap:
+        del ds
 
     for ichunk in range(n_chunks):
 
         channel_start = ichunk * chunksize
-        channel_end = channel_start + chunksize 
+        channel_end = channel_start + chunksize
         if channel_end > maxindex:
-            channel_end = maxindex 
-        
-        ds = load_edax_spd(filename_base) 
-        match axis:
-            case 0: 
-                subsample = np.array(ds['data'][channel_start:channel_end,:,:])
-            case 1: 
-                subsample = np.array(ds['data'][:,channel_start:channel_end,:])
-            case 2: 
-                # worst case!
-                subsample = np.array(ds['data'][:,:,channel_start:channel_end])
+            channel_end = maxindex
 
-        _ = np.sum(subsample)
-        
-    
+        if reload_memmap:
+            ds = load_edax_spd(filename_base)
+
+        match axis:
+            case 0:
+                subsample = np.array(ds['data'][channel_start:channel_end,:,:]).copy()
+            case 1:
+                subsample = np.array(ds['data'][:,channel_start:channel_end,:]).copy()
+            case 2:
+                # worst case!
+                subsample = np.array(ds['data'][:,:,channel_start:channel_end]).copy()
+
+        if reload_memmap:
+            del ds
+
+        # _ = np.sum(subsample)
+        assert len(subsample.shape) == 3
+
+
 
 
 @dataclass
-class cmdArgs: 
-    filename: str 
-    axis: int 
+class cmdArgs:
+    filename: str
+    axis: int
+    reload_memmap: bool
 
 
 if __name__ == "__main__":
@@ -101,9 +110,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--filename', type=str, default='C-12')
     parser.add_argument('--axis', type=int, default=0)
+    parser.add_argument('--reload', type=int, default=0)
     args = parser.parse_args()
-    valid_args = cmdArgs(args.filename, args.axis)
-    run_test(valid_args.filename, valid_args.axis)
+    valid_args = cmdArgs(args.filename, args.axis, bool(args.reload))
+    run_test(valid_args.filename, valid_args.axis, reload_memmap=valid_args.reload_memmap)
 
-    
+
 
--- edax_perf_checks.py
+++ edax_perf_checks.py
@@ -1,6 +1,6 @@
 from rsciio import edax
 import os
-import numpy as np 
+import numpy as np
 from typing import Any
 import numpy.typing as npt
 import argparse
@@ -10,61 +10,90 @@ datadir = os.environ.get("EXTRACTACOMPDATADIR")
 
 
 def load_edax_spd(basename: str) -> dict[str, Any]:
-    subdir = os.path.join(datadir, basename, "Proprietary EDAX Files")    
+    subdir = os.path.join(datadir, basename, "Proprietary EDAX Files")
     fispd = os.path.join(subdir, f"{basename}.spd")
     fiipr = os.path.join(subdir, f"{basename}.ipr")
-    
+
     ds = edax.file_reader(fispd, ipr_fname=fiipr)[0]
-    return ds  
+    return ds
 
 
 
-def sum_over_energy_index_range(filename_base: str, 
-                                index_start: int, 
+def sum_over_energy_index_range(filename_base: str,
+                                index_start: int,
                                 index_end: int,):
-    
-    # do NOT pass the memmap handle in. data still 
-    # accumulates! 
+
+    # do NOT pass the memmap handle in. data still
+    # accumulates!
     # https://stackoverflow.com/a/61472122/9357244
     ds = load_edax_spd(filename_base)
     subsample = np.array(ds['data'][:,:,index_start: index_end])
     return subsample.sum(axis=0).sum(axis=0)
-    
+
 
 def get_n_chunks(filename_base: str, chunksize: int):
-    ds = load_edax_spd(filename_base)  
-    n_chunks = int(np.ceil(ds['axes'][2]['size'] / chunksize))    
+    ds = load_edax_spd(filename_base)
+    n_chunks = int(np.ceil(ds['axes'][2]['size'] / chunksize))
     maxindex = ds['axes'][2]['size']
     return maxindex, n_chunks
 
 def integrate_spatial_dims(filename_base: str, chunksize: int) -> npt.NDArray[np.floating]:
-    
-    maxindex, n_chunks = get_n_chunks(filename_base, chunksize)    
+
+    maxindex, n_chunks = get_n_chunks(filename_base, chunksize)
     channel_sums = np.zeros((maxindex,))
 
     print(f"integrating {n_chunks} chunks")
 
     for ichunk in range(n_chunks):
         channel_start = ichunk * chunksize
-        channel_end = channel_start + chunksize 
+        channel_end = channel_start + chunksize
         if channel_end > maxindex:
-            channel_end = maxindex 
+            channel_end = maxindex
         sumvalues = sum_over_energy_index_range(filename_base, channel_start, channel_end)
         channel_sums[channel_start:channel_end] = sumvalues
 
-    return channel_sums 
+    return channel_sums
 
 
-def run_test(filename_base: str, chunksize: int):        
+def run_test(filename_base: str, chunksize: int):
     channel_sums = integrate_spatial_dims(filename_base, chunksize)
     assert np.all(np.isreal(channel_sums))
-    
 
+def get_dims(filename_base: str):
+    ds = load_edax_spd(filename_base)
+    Nx, Ny, N_channels = ds['data'].shape
+    del ds
+    return int(Nx), int(Ny), int(N_channels)
+
+def get_pixel_energy(filename_base: str, ix:int, iy:int):
+    ds = load_edax_spd(filename_base)
+    value = np.array(ds['data'][ix,iy,:]).copy()
+    del ds
+    return value
+
+def run_pixel_itertest(filename_base: str, max_pixels: int = 50000):
+
+    Nx, Ny, N_channels = get_dims(filename_base)
+
+    channel_energy = np.zeros((Ny, N_channels))
+    i_pixel= 0
+
+    for ix in range(Nx):
+        for iy in range(Ny):
+            if (i_pixel % 10 == 0):
+                print(f"{i_pixel} pixels processed, {Nx*Ny - i_pixel} pixels left of ({Nx,Ny,N_channels})...")
+            value = get_pixel_energy(filename_base, ix, iy)
+            channel_energy += value
+            i_pixel += 1
+            if i_pixel > max_pixels:
+                print("hit max pixels, exiting")
+                return
 
 @dataclass
-class cmdArgs: 
-    filename: str 
-    chunksize: int 
+class cmdArgs:
+    filename: str
+    chunksize: int
+    testcase: str
 
 
 if __name__ == "__main__":
@@ -72,9 +101,32 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--filename', type=str, default='C-12')
     parser.add_argument('--chunksize', type=int, default=50)
+    parser.add_argument('--testcase', type=str, default='spectrum')
     args = parser.parse_args()
-    valid_args = cmdArgs(args.filename, args.chunksize)
-    run_test(valid_args.filename, valid_args.chunksize)
+    valid_args = cmdArgs(filename=args.filename, chunksize=args.chunksize, testcase=args.testcase)
 
-    
+    if valid_args.testcase == 'spectrum':
+        run_test(valid_args.filename, valid_args.chunksize)
+    elif valid_args.testcase == 'load':
+        ds = load_edax_spd(valid_args.filename)
+        assert len(ds['data'].shape) == 3
+        print("loaded file, closing now")
+    elif valid_args.testcase == 'singleaccess':
+        ds = load_edax_spd(valid_args.filename)
+        value = ds['data'][100,100,100]
+        print(f"single value access at (100,100,100) = {value}")
+    elif valid_args.testcase == 'singlemap':
+        ds = load_edax_spd(valid_args.filename)
+        value = np.array(ds['data'][:,:,100])
+        print(f"single map access at (:,:,100) of size {value.shape}")
+    elif valid_args.testcase == 'singlepixel':
+        ds = load_edax_spd(valid_args.filename)
+        value = np.array(ds['data'][500,500,:])
+        sumval = np.sum(value)
+        print(f"single map access at (500,500,:) of size {value.shape}")
+    elif valid_args.testcase == 'pixeliter':
+        run_pixel_itertest(valid_args.filename)
+
+
+
 
